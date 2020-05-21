d4dff884572244af7322745c45e7f413f922fb14
pepedrog <gigeck.pedro@usp.br>
2020-05-21 16:43:28 -0300

    run_measurements com os experimentos de acordo com a tabela1

--- EP1/src/run_measurements.sh
+++ EP1/src/run_measurements.sh
@@ -3,31 +3,29 @@
 set -o xtrace
 
 MEASUREMENTS=13
-ITERATIONS=8
-INITIAL_SIZE=64
-TH_NUM=('32' '64')
+ITERATIONS=9
+INITIAL_SIZE=16
+TH_NUM=('1' '2' '4' '8' '16' '32')
 SIZE=$INITIAL_SIZE
 
-NAMES_TH=('mandelbrot_omp')
-NAMES__=('mandelbrot_seq')
+NAMES_TH=('mandelbrot_omp' 'mandelbrot_pth') 
 make
 
-for NAME in ${NAMES[@]}; do
-    mkdir results/$NAME
+NAME='mandelbrot_seq'
+mkdir results/$NAME
 
-    for ((i=1; i<=$ITERATIONS; i++)); do
-	    perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE>> full.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE>> seahorse.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE>> elephant.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE>> triple_spiral.log 2>&1
-            SIZE=$(($SIZE * 2))
-    done
-
-    SIZE=$INITIAL_SIZE
-
-    mv *.log results/$NAME
-    rm output.ppm
+for ((i=1; i<=$ITERATIONS; i++)); do
+    perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE>> full.log 2>&1
+    perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE>> seahorse.log 2>&1
+    perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE>> elephant.log 2>&1]
+    perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE>> triple_spiral.log 2>&1
+    SIZE=$(($SIZE * 2))
 done
+
+SIZE=$INITIAL_SIZE
+
+mv *.log results/$NAME
+rm output.ppm
 
 for NAME in ${NAMES_TH[@]}; do
     mkdir results/$NAME
