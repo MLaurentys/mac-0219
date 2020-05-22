fb3e4e17b0be56d3574e6f229287e5e7ebba446e
pepedrog <gigeck.pedro@usp.br>
2020-05-22 14:16:39 -0300

    rodei uns testes menores só pra ver os dados

--- EP1/src/run_measurements.sh
+++ EP1/src/run_measurements.sh
@@ -2,33 +2,33 @@
 
 set -o xtrace
 
-MEASUREMENTS=13
-ITERATIONS=9
+MEASUREMENTS=5
+ITERATIONS=7
 INITIAL_SIZE=16
 TH_NUM=('1' '2' '4' '8' '16' '32')
 SIZE=$INITIAL_SIZE
 
 NAMES_TH=('mandelbrot_omp' 'mandelbrot_pth') 
 make
+mkdir results2
 
 NAME='mandelbrot_seq'
-mkdir results/$NAME
+mkdir results2/$NAME
 
 for ((i=1; i<=$ITERATIONS; i++)); do
     perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE>> full.log 2>&1
     perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE>> seahorse.log 2>&1
-    perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE>> elephant.log 2>&1]
+    perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE>> elephant.log 2>&1
     perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE>> triple_spiral.log 2>&1
     SIZE=$(($SIZE * 2))
 done
 
 SIZE=$INITIAL_SIZE
 
-mv *.log results/$NAME
-rm output.ppm
+mv *.log results2/$NAME
 
 for NAME in ${NAMES_TH[@]}; do
-    mkdir results/$NAME
+    mkdir results2/$NAME
     
     for TH in ${TH_NUM[@]}; do
 
@@ -40,10 +40,9 @@ for NAME in ${NAMES_TH[@]}; do
            	 SIZE=$(($SIZE * 2))
     	 done
 
+         SIZE=$INITIAL_SIZE
+
     done
-
-    SIZE=$INITIAL_SIZE
-
-    mv *.log results/$NAME
-    rm output.ppm
+    mv *.log results2/$NAME
 done
+rm output.ppm
