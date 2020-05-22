2a049a119767e5eb6f6f79e9b1c99a7da45a5835
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-05-22 16:32:42 -0300

    fixed size for inter_measurements

--- EP1/src/run_inter_measurements.sh
+++ EP1/src/run_inter_measurements.sh
@@ -4,9 +4,8 @@ set -o xtrace
 
 MEASUREMENTS=5
 ITERATIONS=1
-INITIAL_SIZE=16
 TH_NUM=('8')
-SIZE=$INITIAL_SIZE
+SIZE=1150
 
 NAMES_TH=('mandelbrot_omp' 'mandelbrot_pth') 
 NAME='mandelbrot_seq'
@@ -21,10 +20,8 @@ for ((i=1; i<=$ITERATIONS; i++)); do
     perf record -F 60000 ./$NAME -0.8 -0.7 0.05 0.15 $SIZE && perf report>> seahorse.log 2>&1
     perf record -F 60000 ./$NAME 0.175 0.375 -0.1 0.1 $SIZE && perf report>> elephant.log 2>&1
     perf record -F 60000 ./$NAME -0.188 -0.012 0.554 0.754 $SIZE && perf report>> triple_spiral.log 2>&1
-    SIZE=$(($SIZE * 2))
 done
 
-SIZE=$INITIAL_SIZE
 
 mv *.log results_inter/$NAME
 
@@ -38,12 +35,9 @@ for NAME in ${NAMES_TH[@]}; do
 	   	 perf record -F 60000 ./$NAME -0.8 -0.7 0.05 0.15 $SIZE $TH && perf report>> seahorse$TH.log 2>&1
        		 perf record -F 60000 ./$NAME 0.175 0.375 -0.1 0.1 $SIZE $TH && perf report>> elephant$TH.log 2>&1
 	         perf record -F 60000 ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $TH && perf report>> triple_spiral$TH.log 2>&1
-           	 SIZE=$(($SIZE * 2))
     	 done
-
-         SIZE=$INITIAL_SIZE
-	 
     done
     mv *.log results_inter/$NAME
 done
 rm output.ppm
+rm perf.data*
