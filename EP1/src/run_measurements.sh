c0ee8468b4c085517e531d51e3dadc3eddb24283
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-05-24 19:16:41 -0300

    reverted run_measurements to runa all tests

--- EP1/src/run_measurements.sh
+++ EP1/src/run_measurements.sh
@@ -3,13 +3,30 @@
 set -o xtrace
 
 MEASUREMENTS=13
-ITERATIONS=1
-INITIAL_SIZE=32
-TH_NUM=('32')
+ITERATIONS=10
+INITIAL_SIZE=16
+TH_NUM=('1' '2' '4' '8' '16' '32')
 SIZE=$INITIAL_SIZE
 
-NAMES_TH=('mandelbrot_pth' 'mandelbrot_omp') 
+
+NAMES_TH=('mandelbrot_omp' 'mandelbrot_pth') 
 make
+mkdir results
+
+NAME='mandelbrot_seq'
+mkdir results/$NAME
+
+for ((i=1; i<=$ITERATIONS; i++)); do
+    perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE>> full.log 2>&1
+    perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE>> seahorse.log 2>&1
+    perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE>> elephant.log 2>&1
+    perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE>> triple_spiral.log 2>&1
+    SIZE=$(($SIZE * 2))
+done
+
+SIZE=$INITIAL_SIZE
+
+mv *.log results/$NAME
 
 for NAME in ${NAMES_TH[@]}; do
     mkdir results/$NAME
@@ -27,5 +44,6 @@ for NAME in ${NAMES_TH[@]}; do
          SIZE=$INITIAL_SIZE
 
     done
+    mv *.log results/$NAME
 done
 rm output.ppm
