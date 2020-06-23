c81778b4138e9c7571eaa7cfcc7bf2882a4a3182
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-06-23 16:15:43 -0300

    added task range check for MPI

--- EP2/mandelbrot_mpi.c
+++ EP2/mandelbrot_mpi.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define  MASTER		0
+#define  MASTER	0
 
 int n_threads; // new addition
 
@@ -25,37 +25,36 @@ int image_buffer_size;
 
 int gradient_size = 16;
 int colors[17][3] = {
-                        {66, 30, 15},
-                        {25, 7, 26},
-                        {9, 1, 47},
-                        {4, 4, 73},
-                        {0, 7, 100},
-                        {12, 44, 138},
-                        {24, 82, 177},
-                        {57, 125, 209},
-                        {134, 181, 229},
-                        {211, 236, 248},
-                        {241, 233, 191},
-                        {248, 201, 95},
-                        {255, 170, 0},
-                        {204, 128, 0},
-                        {153, 87, 0},
-                        {106, 52, 3},
-                        {16, 16, 16},
-                    };
+    {66, 30, 15},
+    {25, 7, 26},
+    {9, 1, 47},
+    {4, 4, 73},
+    {0, 7, 100},
+    {12, 44, 138},
+    {24, 82, 177},
+    {57, 125, 209},
+    {134, 181, 229},
+    {211, 236, 248},
+    {241, 233, 191},
+    {248, 201, 95},
+    {255, 170, 0},
+    {204, 128, 0},
+    {153, 87, 0},
+    {106, 52, 3},
+    {16, 16, 16},
+};
 
-void allocate_image_buffer(){
+void allocate_image_buffer() {
     int rgb_size = 3;
     image_buffer = (unsigned char **) malloc(sizeof(unsigned char *) * image_buffer_size);
-
-    for(int i = 0; i < image_buffer_size; i++){
+    for(int i = 0; i < image_buffer_size; i++) {
         image_buffer[i] = (unsigned char *) malloc(sizeof(unsigned char) * rgb_size);
     };
 };
 
-void init(int argc, char *argv[]){
+void init(int argc, char *argv[]) {
     if(argc < 6){
-        printf("usage: ./mpi_mandelbrot c_x_min c_x_max c_y_min c_y_max image_size n_threads \n");
+        printf("usage: ./mandelbrot_mpi c_x_min c_x_max c_y_min c_y_max image_size n_threads \n");
         printf("examples with image_size = 11500:\n");
         printf("    Full Picture:         ./mandelbrot_seq -2.5 1.5 -2.0 2.0 11500\n");
         printf("    Seahorse Valley:      ./mandelbrot_seq -0.8 -0.7 0.05 0.15 11500\n");
@@ -70,7 +69,6 @@ void init(int argc, char *argv[]){
         sscanf(argv[4], "%lf", &c_y_max);
         sscanf(argv[5], "%d", &image_size);
         //sscanf(argv[6], "%d", &n_threads); // new addition
-
 
         i_x_max           = image_size;
         i_y_max           = image_size;
@@ -98,7 +96,7 @@ void update_rgb_buffer(int iteration, int x, int y){
     };
 };
 
-void write_to_file(){
+void write_to_file() {
     FILE * file;
     char * filename               = "output.ppm";
     char * comment                = "# ";
@@ -118,7 +116,6 @@ void write_to_file(){
 };
 
 void compute_mandelbrot(){
-
     double z_x;
     double z_y;
     double z_x_squared;
@@ -132,30 +129,37 @@ void compute_mandelbrot(){
     double c_x;
     double c_y;
 
-    int   numtasks, taskid, chunksize, leftover;
+    int numtasks, taskid, chunksize, leftover;
     int initialized, finalized;
 
-    /***** Initializations *****/
+    // Initializations
     MPI_Initialized(&initialized);
-
     if (!initialized)
        MPI_Init(NULL, NULL);
 
-//////// Perform work in parallel//////////////////////////////////////
-
+    // Perform work in parallel
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-    MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     chunksize = (image_size / numtasks);
     leftover  = (image_size % numtasks);
     MPI_Status status;
 
-    if (taskid == MASTER){
+
+    int rows_per_thread = image_size / numtasks;
+    int rems = image_size - rows_per_thread * numtasks;
+
+    if (taskid == MASTER) {
       printf("mpi_mm has started with %d tasks.\n",numtasks);
     }
+    printf("Task id = %d\n", taskid);
+    int toAdd = (rems - taskid > 0) ? 1 : 0;
+    int tmp = (rems > taskid) ? taskid : rems;
+    int begin_y = taskid * rows_per_thread + tmp;
+    int end_y = begin_y + toAdd + rows_per_thread;
+    printf("doing %d ->= %d\n", begin_y, end_y);
 
-    
-    for(i_y = 0; i_y < i_y_max; i_y++){
-        c_y = c_y_min + i_y * pixel_height;
+    for(int y = begin_y; y < end_y; ++y) {
+        c_y = c_y_min + y * pixel_height;
 
         if(fabs(c_y) < pixel_height / 2)  c_y = 0.0;
 
@@ -175,10 +179,9 @@ void compute_mandelbrot(){
                 z_x_squared = z_x * z_x;
                 z_y_squared = z_y * z_y;
             };
-            update_rgb_buffer(iteration, i_x, i_y);
+            update_rgb_buffer(iteration, i_x, y);
         };
     };
-    
 
     // You also need this when your program is about to exit
     MPI_Finalized(&finalized);
