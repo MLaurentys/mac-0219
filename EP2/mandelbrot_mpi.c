a281ce253d0293190230895d18777b1a3f65df84
HiimHotta <daniel.y.hotta@gmail.com>
2020-06-24 13:57:15 -0300

    seg fault

--- EP2/mandelbrot_mpi.c
+++ EP2/mandelbrot_mpi.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define  MASTER	0
+#define  MASTER		0
 
 int n_threads; // new addition
 
@@ -25,36 +25,44 @@ int image_buffer_size;
 
 int gradient_size = 16;
 int colors[17][3] = {
-    {66, 30, 15},
-    {25, 7, 26},
-    {9, 1, 47},
-    {4, 4, 73},
-    {0, 7, 100},
-    {12, 44, 138},
-    {24, 82, 177},
-    {57, 125, 209},
-    {134, 181, 229},
-    {211, 236, 248},
-    {241, 233, 191},
-    {248, 201, 95},
-    {255, 170, 0},
-    {204, 128, 0},
-    {153, 87, 0},
-    {106, 52, 3},
-    {16, 16, 16},
-};
+                        {66, 30, 15},
+                        {25, 7, 26},
+                        {9, 1, 47},
+                        {4, 4, 73},
+                        {0, 7, 100},
+                        {12, 44, 138},
+                        {24, 82, 177},
+                        {57, 125, 209},
+                        {134, 181, 229},
+                        {211, 236, 248},
+                        {241, 233, 191},
+                        {248, 201, 95},
+                        {255, 170, 0},
+                        {204, 128, 0},
+                        {153, 87, 0},
+                        {106, 52, 3},
+                        {16, 16, 16},
+                    };
 
-void allocate_image_buffer() {
+void allocate_image_buffer(){
     int rgb_size = 3;
     image_buffer = (unsigned char **) malloc(sizeof(unsigned char *) * image_buffer_size);
-    for(int i = 0; i < image_buffer_size; i++) {
+
+    for(int i = 0; i < image_buffer_size; i++){
         image_buffer[i] = (unsigned char *) malloc(sizeof(unsigned char) * rgb_size);
     };
 };
 
-void init(int argc, char *argv[]) {
+void free_image_buffer () {
+    for(int i = 0; i < image_buffer_size; i++){
+        free (image_buffer[i]);
+    };
+    free (image_buffer);
+};
+
+void init(int argc, char *argv[]){
     if(argc < 6){
-        printf("usage: ./mandelbrot_mpi c_x_min c_x_max c_y_min c_y_max image_size n_threads \n");
+        printf("usage: ./mpi_mandelbrot c_x_min c_x_max c_y_min c_y_max image_size n_threads \n");
         printf("examples with image_size = 11500:\n");
         printf("    Full Picture:         ./mandelbrot_seq -2.5 1.5 -2.0 2.0 11500\n");
         printf("    Seahorse Valley:      ./mandelbrot_seq -0.8 -0.7 0.05 0.15 11500\n");
@@ -70,6 +78,7 @@ void init(int argc, char *argv[]) {
         sscanf(argv[5], "%d", &image_size);
         //sscanf(argv[6], "%d", &n_threads); // new addition
 
+
         i_x_max           = image_size;
         i_y_max           = image_size;
         image_buffer_size = image_size * image_size;
@@ -79,7 +88,7 @@ void init(int argc, char *argv[]) {
     };
 };
 
-void update_rgb_buffer(int iteration, int x, int y){
+unsigned char** update_rgb_buffer(int iteration, int x, int y){
     int color;
 
     if(iteration == iteration_max){
@@ -94,9 +103,11 @@ void update_rgb_buffer(int iteration, int x, int y){
         image_buffer[(i_y_max * y) + x][1] = colors[color][1];
         image_buffer[(i_y_max * y) + x][2] = colors[color][2];
     };
+
+    //return image_buffer;
 };
 
-void write_to_file() {
+void write_to_file(){
     FILE * file;
     char * filename               = "output.ppm";
     char * comment                = "# ";
@@ -115,7 +126,8 @@ void write_to_file() {
     fclose(file);
 };
 
-void compute_mandelbrot(){
+void compute_mandelbrot (int begin_y, int end_y, unsigned char** image_buffer){
+
     double z_x;
     double z_y;
     double z_x_squared;
@@ -129,37 +141,8 @@ void compute_mandelbrot(){
     double c_x;
     double c_y;
 
-    int numtasks, taskid, chunksize, leftover;
-    int initialized, finalized;
-
-    // Initializations
-    MPI_Initialized(&initialized);
-    if (!initialized)
-       MPI_Init(NULL, NULL);
-
-    // Perform work in parallel
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-    chunksize = (image_size / numtasks);
-    leftover  = (image_size % numtasks);
-    MPI_Status status;
-
-
-    int rows_per_thread = image_size / numtasks;
-    int rems = image_size - rows_per_thread * numtasks;
-
-    if (taskid == MASTER) {
-      printf("mpi_mm has started with %d tasks.\n",numtasks);
-    }
-    printf("Task id = %d\n", taskid);
-    int toAdd = (rems - taskid > 0) ? 1 : 0;
-    int tmp = (rems > taskid) ? taskid : rems;
-    int begin_y = taskid * rows_per_thread + tmp;
-    int end_y = begin_y + toAdd + rows_per_thread;
-    printf("doing %d ->= %d\n", begin_y, end_y);
-
-    for(int y = begin_y; y < end_y; ++y) {
-        c_y = c_y_min + y * pixel_height;
+    for(i_y = begin_y; i_y < end_y; i_y++){
+        c_y = c_y_min + i_y * pixel_height;
 
         if(fabs(c_y) < pixel_height / 2)  c_y = 0.0;
 
@@ -179,25 +162,121 @@ void compute_mandelbrot(){
                 z_x_squared = z_x * z_x;
                 z_y_squared = z_y * z_y;
             };
-            update_rgb_buffer(iteration, i_x, y);
+            update_rgb_buffer(iteration, i_x, i_y);
         };
     };
-
-    // You also need this when your program is about to exit
-    MPI_Finalized(&finalized);
-    if (!finalized)
-       MPI_Finalize();
-
+   //return image_buffer;
 };
 
+
 int main(int argc, char *argv[]){
-    init(argc, argv);
+    int size, rank, chunksize, leftover, n, begin_y, end_y, tag1, tag2, source, dest;
 
-    allocate_image_buffer();
+    MPI_Status status;
 
-    compute_mandelbrot();
+    init (argc, argv);
 
-    write_to_file();
+    //compute_mandelbrot();
+
+    /***** Initializations *****/
+    /*
+    MPI_Initialized(&initialized);
+
+    if (!initialized)
+       MPI_Init(NULL, NULL);
+    */
+
+    MPI_Init (&argc, &argv);
+
+//////// Perform work in parallel//////////////////////////////////////
+
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+    MPI_Comm_rank (MPI_COMM_WORLD,&rank);
+
+    chunksize = i_y_max / size;
+    leftover  = i_y_max % size; //cant be distributed equally if > 0
+    //send and receive tags
+    tag2 = 1;
+    tag1 = 2;
+
+    // MASTER TASK
+    if (rank == MASTER){
+        allocate_image_buffer();
+        printf("mpi_mm has started with %d tasks.\n",size);
+    
+        /* Send each task its portion of the work, some more than others (+1) */
+        begin_y = chunksize + leftover;
+        end_y = begin_y + chunksize;
+        for (dest = 1; dest < size; dest++) {
+            end_y = (dest <= leftover) ? end_y++ : end_y;
+            MPI_Send (&begin_y, 1, MPI_INT, dest, tag1, MPI_COMM_WORLD);
+            MPI_Send (&end_y  , 1, MPI_INT, dest, tag1, MPI_COMM_WORLD);
+            MPI_Send (&image_buffer[begin_y][0], chunksize * i_x_max, MPI_UNSIGNED_CHAR, dest, tag2, MPI_COMM_WORLD);
+            printf ("Sent %d elements to task %d offset= %d\n", chunksize, dest, begin_y);
+            begin_y += chunksize;
+            end_y += chunksize;
+        }
+
+        /* Master does its part of the work */
+        //end_y = begin_y + chunksize + leftover;
+        //mysum = update (offset, chunksize+leftover, taskid);
+        compute_mandelbrot (0, chunksize, image_buffer);
+
+        /* Wait to receive results from each task */
+        for (int i = 1; i < size; i++) {
+            source = i;
+            MPI_Recv (&begin_y, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
+            MPI_Recv (&end_y  , 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
+            //MPI_Recv(&image_buffer[begin_y][0], chunksize * i_x_max, MPI_UNSIGNED_CHAR, source, tag2, MPI_COMM_WORLD, &status);
+        }
+
+        /* Get final sum and print sample results */
+        //MPI_Reduce(&mysum, &sum, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+
+        printf ("Writing to file........ \n");
+
+        write_to_file();
+    }  /* end of master section */
+
+
+/*
+    for(i=loop_min;i<loop_max;i++) {
+        for(j=0;j<DIM;j++) {
+         dot=0.0;
+         for(k=0;k&;lt;DIM;k++)
+           dot += a[i][k]*b[k][j];
+         c[i][j]=dot;
+       }
+
+     }
+
+*/
+
+    //WORKER TASK
+    if (rank > MASTER) {
+        /* Receive my portion of array from the master task */
+        source = MASTER;
+        MPI_Recv (&begin_y, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
+        MPI_Recv (&end_y  , 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
+        MPI_Recv (&image_buffer[begin_y], chunksize, MPI_UNSIGNED_CHAR, source, tag2, MPI_COMM_WORLD, &status);
+
+        /* Do my part of the work */
+
+        //mysum = update(offset, chunksize, taskid);
+        //compute_mandelbrot (begin_y, end_y, image_buffer);
+
+        /* Send my results back to the master task */
+        dest = MASTER;
+        MPI_Send(&begin_y, 1, MPI_INT, dest, tag1, MPI_COMM_WORLD);
+        MPI_Send(&end_y  , 1, MPI_INT, dest, tag1, MPI_COMM_WORLD);
+        //MPI_Send(&image_buffer[begin_y], chunksize, MPI_UNSIGNED_CHAR, MASTER, tag2, MPI_COMM_WORLD);
+
+        /* Use sum reduction operation to obtain final sum */
+        //MPI_Reduce(&image_buffer, &image_buffer, 1, MPI_UNSIGNED_CHAR, MPI_SUM, MASTER, MPI_COMM_WORLD);
+
+    } /* end of non-master */
+
+    MPI_Finalize();
 
     return 0;
 };
