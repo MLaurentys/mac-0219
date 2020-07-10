2f6feb3a9f54287016c20c4b63472261ea5b5065
pepedrog <gigeck.pedro@usp.br>
2020-07-10 18:56:38 -0300

    files from EP1 and notebook with final graph and conclusion (waiting ompi+omp)

--- EP2/run_measurements.sh
+++ EP2/run_measurements.sh
@@ -6,8 +6,6 @@ MEASUREMENTS=15
 SIZE=4096
 
 # CUDA parameters
-GRIDS=('4' '4' '16' '16' '64' '64' '256' '256' '1024' '1024' '2048' '2048')
-GRIDS_NAME=('4_4' '16_16' '64_64' '256_256' '1024_1024' '2048_2048')
 CUDA_BLOCKS=('1' '512' '1024' '2048')
 THREADS=('4')
 CUDA_RUN="./bin/mandelbrot_cuda"
@@ -17,17 +15,18 @@ NUM_TASKS=('4' '8' '16' '32' '64')
 MPI_RUN="mpirun -np"
 MPI_BIN="./bin/mandelbrot_ompi"
 
+# OMPI+OMP parameters
+NUM_THREADS=('1', '2', '4', '8', '16', '32', '64', '128', '256', '512', '1024', '2048')
+MPI_RUN="mpirun -np"
+MPI_OMP_BIN="./bin/mandelbrot_ompi_omp"
+
 MKDIR='mkdir -p'
 
-#make
+make
 ${MKDIR} results
 ${MKDIR} results/cuda
 ${MKDIR} results/ompi
-
-# for ((i = 0; i < ${#GRIDS[@]}; i+=2)) do
-#    perf stat -r $MEASUREMENTS $CUDA_RUN -0.188 -0.012 0.554 0.754 $SIZE "${GRIDS[$i]}"\
-#        "${GRIDS[$i+1]}">> triple_spiral_${GRIDS_NAME[$i/2]}.log 2>&1
-# done
+${MKDIR} results/ompi_omp
 
 for BLOCKS in ${CUDA_BLOCKS[@]}; do
     for ((THREADS=4; THREADS<=2048; THREADS*=2)); do
@@ -43,5 +42,13 @@ for ((i = 0; i < ${#NUM_TASKS[@]}; i+=1)) do
     >> triple_spiral_${NUM_TASKS[$i]}.log 2>&1
 done
 mv *.log results/ompi/
+
+for ((i = 0; i < ${#NUM_TASKS[@]}; i+=1)) do
+    for ((THREADS=1; THREADS<=2048; THREADS*=2)); do
+        perf stat -r $MEASUREMENTS $MPI_RUN ${NUM_TASKS[$i]} $MPI_OMP_BIN -0.188 -0.012 0.554 0.754 \
+        $SIZE $THREADS >> triple_spiral_${NUM_TASKS[$i]}_"$THREADS".log 2>&1
+    done
+done
+mv *.log results/ompi_omp/
 
 rm output.ppm
